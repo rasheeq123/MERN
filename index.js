26ac7183fab7e4b92ffb515a4219d4f4d22ed40a
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-08-21 12:20:36 +0530

    JS

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("hello");
+console.log("world");
+myname="Rasheeq";
+num=30;
+console.log(num);
+
+console.log(num+'3');
+console.log( typeof num); //know the type of data type
+isLoggedIn=true;  //camel-case
+console.log(typeof isLoggedIn)
+age=23;
+if(age>18)
+{
+console.log("eligible for licence")
+}
+else
+{
+console.log("not eligible for licence")
+}
+
+process.stdout.write("the command will be printed in same line")
+console.log("break")
+ console.log(999=='999')
+ console.log(999==='999')
+ console.log(999===999)
+console.log(56+34+'32')
+console.log(4**2);
+console.log(25**0.5);
---
+++ question.js
@@ -0,0 +1,22 @@
+//WAP if a number is divisible by 7 and 11
+
+
+n1=98;
+if(n1%7==0 && n1%11==0)
+{
+    console.log("the number is divided by 7 and 11")
+}
+else if(n1%7==0 )
+{
+    console.log("divided by 7 ")
+}
+else if(n1%11==0 )
+{
+    console.log("divided by 7 ")
+}
+else
+{
+console.log("not divisible 7 and 11")
+}
+
+    // node -- watch (filename) : command for everytime we create a new file
