eabb386dfaa2bbdab3452137ad01d49b7a020d17
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-08-26 15:58:00 +0530

    js

---
+++ assignment.js
@@ -0,0 +1,10 @@
+//WAP to square all elements of an array and store them into a new array.
+const nums=[12,13,14,15];
+let new=[];
+
+for(i=0; i<nums.length; i++)
+{
+    nums[i]=nums[i]**2;
+}
+new.push(nums[i])
+console.log(new)
